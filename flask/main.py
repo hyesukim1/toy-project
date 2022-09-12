b4a9c1f7e9ca0350bc4d3f9a5cbd97376a3c32f3
AI_09_김혜수 <94281700+hyesukim1@users.noreply.github.com>
2022-09-12 17:44:18 +0900

    project

---
+++ flask/main.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, Response, jsonify
+import base64
+from flask_cors import CORS
+import numpy as np
+import cv2
+from PIL import Image
+from io import BytesIO
+
+confthres=0.5
+nmsthres=0.1
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/dnn/yolo', methods=['POST'])
+def main():
+    model = request.form['model']
+    if model == 'apple':    
+        labelsPath="C:/Users/kimsu/wed-dnn/flask/model/classes.names"   
+        configpath="C:/Users/kimsu/wed-dnn/flask/model/apple-train-yolo.cfg" 
+        weightspath="C:/Users/kimsu/wed-dnn/flask/model/apple-train-yolo_final.weights"  
+    else:
+        labelsPath="C:/Users/kimsu/wed-dnn/flask/model/coco.names"   
+        configpath="C:/Users/kimsu/wed-dnn/flask/model/yolov3.cfg" 
+        weightspath="C:/Users/kimsu/wed-dnn/flask/model/yolov3.weights" 
+    print("[INFO] loading ", model.upper(), " models...")
+    LABELS = open(labelsPath).read().strip().split("\n")
+    net = cv2.dnn.readNet(configpath, weightspath) 
+
+    file = request.form['image']
+    starter = file.find(',')
+    image_data = file[starter+1:]
+    image_data = bytes(image_data, encoding="ascii")
+    img = Image.open(BytesIO(base64.b64decode(image_data))) 
+    #img = cv2.imread('./dog.jpg')
+    npimg=np.array(img)
+    image=npimg.copy()
+    image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    (H, W) = image.shape[:2]
+
+    ln = net.getLayerNames()
+    outPlayer = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(outPlayer)
+
+    boxes = []
+    confidences = []
+    classes = []
+    results = [] 
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            if confidence > confthres:
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classes.append({ 'id': int(classID), 'name': LABELS[classID] })
+
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres,
+                            nmsthres)
+
+    if len(idxs) > 0:
+        for i in idxs.flatten():
+            results.append({ 'class': classes[i], 'confidence': confidences[i], 'bbox': boxes[i] })
+
+    return jsonify(results)
+
+# start flask app
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,110 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+  @ViewChild('video', { static: false}) videoElement: ElementRef;
+  @ViewChild('canvas', { static:false}) canvas: ElementRef;
+
+  videoBoundingRect;
+  videoWidth: number = 0;
+  videoHeight: number = 0;
+  constraints = {
+    video:{
+      facingMode: "environment"
+    }
+  };
+
+  model: string = 'yolo'
+
+  showVideo: boolean = false;
+  svgEnabled: boolean = true;
+  doSpinner: boolean = false;
+  apiUrl = "https://central-insight-361802.du.r.appspot.com"
+
+  currentDetections;
+
+  constructor(private renderer: Renderer2,
+    private http: HttpClient) {}
+
+  ngOnInit(): void {
+      this.startCamera();
+  }
+
+  async startCamera() {
+    if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
+        navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
+        this.showVideo = true;
+        this.svgEnabled = false;
+    } else {
+        alert('Sorry, camera not available.');
+    }
+  }
+  
+  attachVideo(stream) {
+    this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
+    this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
+        this.videoHeight = this.videoElement.nativeElement.videoHeight;
+        this.videoWidth = this.videoElement.nativeElement.videoWidth;
+    });
+  }
+  handleError(error) {
+    console.log('Error: ', error);
+    alert('Error: '+ error);
+  }
+  async onVideoCanPlay() {
+    this.videoBoundingRect = this.videoElement.nativeElement.getBoundingClientRect();
+  }
+  changeModel(event: any) {
+    this.model = event.target.value;
+  }
+  onFileChange(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.svgEnabled = false;
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      const img = new Image();
+
+      img.onload = () => {
+        this.showVideo = false;
+        this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
+        this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
+        this.canvas.nativeElement.getContext('2d').drawImage(img, 0, 0)
+      };
+      img.src = URL.createObjectURL(file);
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+
+        this.getDetections(reader.result);
+      };
+    }
+  }
+  async capture() {
+    this.showVideo = false;
+    this.renderer.setProperty(this.canvas.nativeElement, 'width', this.videoWidth);
+    this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
+    this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0)
+    
+    console.log(this.canvas.nativeElement.toDataURL())
+    this.getDetections(this.canvas.nativeElement.toDataURL());
+    
+  }
+  getDetections(image: any) {
+    this.doSpinner = true;
+    const formData = new FormData();
+    formData.append('image', image);
+    formData.append('model', this.model);
+
+    this.http.post(`${this.apiUrl}/dnn/yolo`, formData)
+      .subscribe(res => {
+        this.currentDetections = res;
+        this.svgEnabled = true;
+        this.doSpinner = false;
+      });
+  }
+}
+
